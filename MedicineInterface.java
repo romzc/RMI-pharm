c03825a01cd7c75f2f7089aa748d2aa3e0fd1c20
Rony <ronyramos495@gmail.com>
2022-06-08 09:42:35 -0500

    Resolucion del ejerccio de RMI

---
+++ MedicineInterface.java
@@ -0,0 +1,9 @@
+package Medicinas;
+import java.rmi.Remote;
+
+public interface MedicineInterface extends Remote {
+	
+	public Medicine getMedicine( int amount ) throws Exception;
+	public int getStock() throws Exception;
+	public String print() throws Exception;
+}
